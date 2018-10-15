81deb36254ab8079c61b2226391064e8382c06f7
ContentValues <736126375@qq.com>
2018-10-15 16:38:43 +0800

    fix

---
+++ app/src/main/java/xdroid/mwee/com/zmstudy/view/XXAView.java
@@ -0,0 +1,24 @@
+package xdroid.mwee.com.zmstudy.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by zhangmin on 2018/10/15.
+ */
+
+public class XXAView extends View {
+    public XXAView(Context context) {
+        super(context);
+    }
+
+    public XXAView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public XXAView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
