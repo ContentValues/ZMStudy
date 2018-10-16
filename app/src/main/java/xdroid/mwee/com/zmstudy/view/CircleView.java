5ecccb88881dd91cdeb7c0e390b420261917a4cb
ContentValues <736126375@qq.com>
2018-10-16 10:46:18 +0800

    update

--- app/src/main/java/xdroid/mwee/com/zmstudy/view/CircleView.java
+++ app/src/main/java/xdroid/mwee/com/zmstudy/view/CircleView.java
@@ -70,6 +70,8 @@ public class CircleView extends View {
 
         LogUtil.log("CircleView","git add .");
 
+        LogUtil.log("CircleView","git iml .");
+
         canvas.save();
 
         //int r = getMeasuredWidth() / 2;//也可以是getMeasuredHeight()/2,本例中我们已经将宽高设置相等了
