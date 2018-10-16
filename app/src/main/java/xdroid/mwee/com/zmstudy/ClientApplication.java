a54ef02d9a4dab8779c5925df3267d1a334656d2
ContentValues <736126375@qq.com>
2018-10-16 11:06:24 +0800

    update

--- app/src/main/java/xdroid/mwee/com/zmstudy/ClientApplication.java
+++ app/src/main/java/xdroid/mwee/com/zmstudy/ClientApplication.java
@@ -45,6 +45,8 @@ public class ClientApplication extends Application {
         //BaseConfig.ENV = Environment.DEV;
         //BaseConfig.ENV = Environment.DEV;
         //BaseConfig.ENV = Environment.DEV;
+        //BaseConfig.ENV = Environment.DEV;
+        //BaseConfig.ENV = Environment.DEV;
 
         GlobalCache.getInstance().registerContext(context);
         GlobalCache.getInstance().setLogOpen(BaseConfig.ENV != Environment.PRODUCT);
