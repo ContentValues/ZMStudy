ec8f0b162ea7a5f6ffe9a2921ad2a4786d37184a
张敏 <736126375@qq.com>
2019-03-11 17:17:05 +0800

    测试2

--- app/src/main/java/xdroid/mwee/com/zmstudy/ui/fragment/BasePagerFragment.java
+++ app/src/main/java/xdroid/mwee/com/zmstudy/ui/fragment/BasePagerFragment.java
@@ -43,7 +43,7 @@ public abstract class BasePagerFragment extends BaseFragment {
         tv_error = v.findViewById(R.id.tv_error);
         tv_empty = v.findViewById(R.id.tv_empty);
 
-        System.out.println("测试1");
+        System.out.println("测试2");
     }
 
     @Override
